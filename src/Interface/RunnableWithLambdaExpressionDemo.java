1c33c54584e128568794cc9006ddcc7f39d81117
anjali.bz.kumari <anjali.bz.kumari@bdc7-l-g04gdeg.dir.svc.accenture.com>
2025-04-19 15:45:21 +0530

    Main FB-B1May25DL-2860

--- src/Interface/RunnableWithLambdaExpressionDemo.java
+++ src/Interface/RunnableWithLambdaExpressionDemo.java
@@ -36,6 +36,9 @@ public class RunnableWithLambdaExpressionDemo {
 		t2.start();
 		
 		System.out.println("Main FB-B1May25DL-2860");
+		System.out.println("Main FB-B1May25DL-2860");
+		System.out.println("Main FB-B1May25DL-2860");
+		System.out.println("Main FB-B1May25DL-2860");
 	}
 
 }
