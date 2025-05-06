32b09b2796e284d5dd9d4924c7e5235c5ba4eb4f
anjali.bz.kumari <anjali.bz.kumari@bdc7-l-g04gdeg.dir.svc.accenture.com>
2025-05-06 12:25:16 +0530

    Raise Pull request check

---
+++ src/Interface/RaisePullrequestCheck.java
@@ -0,0 +1,11 @@
+package Interface;
+
+public class RaisePullrequestCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Raise Pull Request");
+
+	}
+
+}
